d7564927b52a5cb14e81133dbe466d00fe660404
AlejandroBudy <atorresato@gmail.com>
2021-12-02 19:02:54 +0100

    Day 01: Part 2 solution implementation

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,15 @@
 fun main() {
-    fun part1(input: List<String>): Int =
-        input.windowed(2).count {
-            it.first().toInt() < it.last().toInt()
-        }
-
-
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int = input.windowed(2).count { (a, b) -> a < b }
+    fun part2(input: List<Int>): Int = input.windowed(3).windowed(2).count { (a, b) ->
+        a.sum() < b.sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInputAsInt("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputAsInt("Day01")
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
